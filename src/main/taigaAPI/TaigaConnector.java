652e6fb4c18df68357641a63d97ea7f0c49b926c
jjos14 Jacob Jose <jjose14@asu.edu>
2022-04-04 02:17:19 -0700

    Successfully converted a string to JSON

--- src/main/taigaAPI/TaigaConnector.java
+++ src/main/taigaAPI/TaigaConnector.java
@@ -1,5 +1,9 @@
 package taigaAPI;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,12 +13,41 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.net.http.HttpClient;
+import java.text.ParseException;
 
 public class TaigaConnector {
-    private URL url;
-    private HttpURLConnection httpURLConnection;
 
-    public
+    public static JSONObject getResponseFromUrlAsJson (String url, String requestMethodType, String requestMessageString) {
+
+        URL url = new URL ("https://reqres.in/api/users");
+
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+        String jsonInputString = "{\"name\" \"Upendra\", \"job\": \"Programmer\"}";
+
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+            JSONObject jsonObject = new JSONObject(response.toString());
+            System.out.printf(jsonObject.toString());
+        }
+
+        return null;
+    }
+
 
 
     public static void main (String [] args) throws IOException {
@@ -40,6 +73,8 @@ public class TaigaConnector {
                 response.append(responseLine.trim());
             }
             System.out.println(response.toString());
+            JSONObject jsonObject = new JSONObject(response.toString());
+            System.out.printf(jsonObject.toString());
         }
 
 
